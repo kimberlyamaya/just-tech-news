59a970195344fe462281c5ab74e9128f72286b30
kimberlyamaya <kimberly.kimbell89@gmail.com>
2022-01-06 18:44:25 -0800

    Create and manipulate comments on post data

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -11,8 +11,12 @@ router.get('/', (req, res) => {
       order: [['created_at', 'DESC']], 
       include: [
         {
-          model: User,
-          attributes: ['username']
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
         }
       ]
     })
